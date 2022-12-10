876abab58363966f1bee87e18ca0c689a83c019a
besimsherifi <sherifibesim@gmail.com>
2022-12-10 14:26:03 +0100

    commit

---
+++ src/lib/apollo.js
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+//   uri: "https://testajredin.000webhostapp.com/graphql",
+uri:"http://wptest.x10.mx/graphql",
+  cache: new InMemoryCache()
+});
+
+export default client;
