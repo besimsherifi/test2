ccd6a744a76580f986a7c1e66a81ce003105b8dd
besimsherifi <sherifibesim@gmail.com>
2022-12-10 14:41:13 +0100

    trying

--- src/lib/apollo.js
+++ src/lib/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
 //   uri: "https://testajredin.000webhostapp.com/graphql",
-uri:"http://wptest.x10.mx/graphql",
+uri:"https://wptest.x10.mx/graphql",
   cache: new InMemoryCache()
 });
 
